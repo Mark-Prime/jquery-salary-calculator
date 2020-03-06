e37e74855e118577ec62023c61e151f781d6475e
Mark Spannbauer <msmarkieer@gmail.com>
2020-03-06 15:44:23 -0600

    Added functionality

--- assignment/client.js
+++ assignment/client.js
@@ -1,12 +1,13 @@
 console.log('js');
 
-let totalSalary = 0;
+let totalSalary = 12000000100;
 
 $(readyNow);
 
 function readyNow() {
     console.log('jQ');
-    $('#btn-submit').on('click', addToTable)
+    $('#btn-submit').on('click', addToTable);
+    $('.btn-table').on('click', deleteTableRow);
 }
 
 // Adds the items from the inputs to the table below
@@ -36,12 +37,32 @@ function addToTable( event ) {
         $('#in-salary').val('');
 
         // Adds the values to the table
-        $('.container').append(`<tr><td>${firstName}</td><td>${lastName}</td><td>#${id}</td><td>${title}</td><td>$${salary}</td></tr>`)
-
+        $('#main-table').append(`<tr id="${firstName}-${id}" ><td>${firstName}</td><td>${lastName}</td><td>#${id}</td><td>${title}</td><td class="salaryCell">$${salary}</td><td><button type="button" class="btn-table" id="${firstName}-${id}">Delete</button></td></tr>`);
+        $('.btn-table').on('click', deleteTableRow);
         // Adds value to the total value
         totalSalary += Number(salary);
         $('h3').text(`Total Salary: $${totalSalary}`);
 
     }// Ends if all filled
     
+}
+
+function deleteTableRow(event) {
+    console.log('event:', event.target.id);
+    let eventID = event.target.id;
+    let eventLine = $(`#${eventID}`);
+
+    eventLine.each(function () {
+        let salary = $(this).find(".salaryCell").html();
+        salary = Number(salary.slice(1));
+        console.log('salary', salary);
+        totalSalary -= salary;
+        console.log('total salary', totalSalary);
+
+        
+        $('h3').text(`Total Salary: $${totalSalary}`);
+        
+    });
+
+    eventLine.remove();
 }
